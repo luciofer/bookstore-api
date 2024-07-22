1710bb380bcfbafb914423bb559110e1e270c311
Lucio Araujo <contatolucioaraujo@gmail.com>
2024-07-22 17:18:11 -0300

    Created the project

---
+++ src/test/kotlin/com/lfdev/bookstore/BookstoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lfdev.bookstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
