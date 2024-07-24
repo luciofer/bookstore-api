9e96fb63db0e7215a91a601ad1db34ab5b85095a
Lucio Araujo <contatolucioaraujo@gmail.com>
2024-07-23 23:53:01 -0300

    Add migrations with flyway

---
+++ src/main/kotlin/com/lfdev/bookstore/repositories/BookRepository.kt
@@ -0,0 +1,9 @@
+package com.lfdev.bookstore.repositories
+
+import com.lfdev.bookstore.domain.Book
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface BookRepository : JpaRepository<Book, String> {
+}
